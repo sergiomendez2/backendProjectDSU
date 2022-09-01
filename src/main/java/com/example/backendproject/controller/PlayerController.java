f8e1f3b38cb0c474cb01abf953bd6a0f8b4b1490
sergio.mendez2 <sergio.mendez2@telusinternational.com>
2022-09-01 11:19:17 -0600

    nothing was done

--- src/main/java/com/example/backendproject/controller/PlayerController.java
+++ src/main/java/com/example/backendproject/controller/PlayerController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/app/v1")
-@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT})
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class PlayerController {
 
 	@Autowired
